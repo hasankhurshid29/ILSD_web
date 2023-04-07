ec9a62c39a5019c47012166ba9a99d5f7978566e
Taimur Israr <40432974+taymurisrar@users.noreply.github.com>
2023-04-07 20:27:43 +0500

    initial

---
+++ js/global.js
@@ -0,0 +1,150 @@
+const div = `
+<div class="collapse navbar-collapse text-center" id="navbarsExample09">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="/index.html">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item"><a class="nav-link" href="/index.html#about">About</a></li>
+						<li class="nav-item"><a class="nav-link" href="/index.html#service">Services</a></li>
+						<li class="nav-item"><a class="nav-link" href="/pages/blog.html">Blog</a></li>
+						<li class="nav-item"><a class="nav-link" href="/contact.html">Contact</a></li>
+					</ul>
+				</div>
+
+`;
+
+const disv = `
+
+<div class="sidebar-widget latest-post card border-0 p-4 mb-3">
+								<h5>Recent Blogs</h5>
+								<div class="media border-bottom py-3">
+								<a href="#"><img class="mr-4" src="/images/blog/tb-asthma.jpg" alt=""></a>
+								<div class="media-body">
+									<h6><a href="/pages/asthma-management-plan.html">ASTHMA MANAGEMENT PLAN</a></h6>
+									<span class="text-sm text-muted">24 February 2022</span>
+								</div>
+							</div>
+								<div class="media border-bottom py-3">
+									<a href="#"><img class="mr-4" src="/images/blog/tb-why-do-we-sleep.jpg" alt=""></a>
+									<div class="media-body">
+										<h6><a href="/pages/sleep-secrets.html">WHY DO WE SLEEP?</a></h6>
+										<span class="text-sm text-muted">07 January 2022</span>
+									</div>
+								</div>
+
+								<div class="media border-bottom py-3">
+									<a href="#"><img class="mr-4" src="/images/blog/tb-lungs-cleanse.jpg" alt=""></a>
+									<div class="media-body">
+										<h6><a href="/pages/what-you-should-know-about-lung-diseases.html">ONE LUNG
+												CLEANSER A DAY, KEEPS ANY PULMONARY DISORDER AWAY</a></h6>
+										<span class="text-sm text-muted">06 December 2021</span>
+									</div>
+								</div>
+
+								<div class="media border-bottom py-3">
+									<a href="#"><img class="mr-4" src="/images/blog/blogthumb4.jpg" alt=""></a>
+									<div class="media-body">
+										<h6><a href="/pages/what-you-should-know-about-lung-diseases.html">WHAT YOU
+												SHOULD KNOW ABOUT LUNG DISEASES, AND HOW TO PROTECT YOUR LUNGS</a></h6>
+										<span class="text-sm text-muted">03 November 2021</span>
+									</div>
+								</div>
+
+							</div>
+
+`;
+
+
+const footer = `
+
+<footer class="footer section">
+<div class="container">
+	<div class="row">
+		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+			<div class="widget">
+				<a class="mb-2" href="https://ilungsleep.com:2096/">
+					<img src="/images/logo.png" alt="Institute of Lung and Sleep Disorder"
+						title="Institute of Lung and Sleep Disorder" class="diji-logo mb-4" />
+				</a>
+				<ul class="social-icons list-inline mt-2">
+					<li class="list-inline-item">
+						<a href="https://www.facebook.com/ilungsleepofficial" target="_blank"><i
+								class="ti-facebook"></i></a>
+					</li>
+					<li class="list-inline-item">
+						<a href="https://twitter.com/ilungsleep" target="_blank"><i
+								class="ti-twitter"></i></a></a>
+					</li>
+					<li class="list-inline-item">
+						<a href="https://www.instagram.com/ilungsleepofficial/" target="_blank"><i
+								class="ti-instagram"></i></a>
+					</li>
+					<li class="list-inline-item">
+						<a href="https://www.linkedin.com/company/ilungsleepofficial/" target="_blank"><i
+								class="ti-linkedin"></i></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="col-lg-5 col-md-4 col-sm-6 col-xs-12">
+			<div class="widget">
+				<h4 class="text-capitalize mb-4">Company</h4>
+				<ul class="list-unstyled footer-menu lh-35">
+					<li><i class="fa fa-map-marker mr-3" aria-hidden="true"></i>321 N Highland Ave STE 105
+						Sherman, TX</li>
+					<li><a href="mailto:contact@ilungsleep.com"><i
+								class="fa fa-envelope mr-3"></i>info@ilungsleep.com</a></li>
+					<li><a href="tel:+1 (903) 347-0001"><i
+								class="fa fa-flip-horizontal fa-phone mr-3"></i>+1
+							(903) 347-0001</a></li>
+					<li><a href="tel:+1 (903) 347-0002"><i class="fa fa-fax mr-3"></i>903-347-0002</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+			<div class="widget">
+				<h4 class="text-capitalize mb-4">Hours</h4>
+				<ul class="list-unstyled footer-menu lh-35">
+					<li>
+						<p>Monday - Thursday </p>
+						<span>08:30 AM - 4:30 PM</span>
+					</li>
+					<li>
+						<p>Friday</p>
+						<span> 08:30 AM - 12:00 PM</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="footer-btm pt-4 pb-1 pl-4 pr-4">
+	<div class="row">
+		<div class="col-lg-6">
+			<div class="copyright">
+				&copy; Copyright Reserved. POWERED BY <a href="https://dijisync.com/"
+					target="_blank">DijiSync
+					HCS LLC</a>
+			</div>
+		</div>
+		<div class="col-lg-6 text-left text-lg-right">
+			<ul class="list-inline footer-socials">
+				<li class="list-inline-item"><a href="https://www.facebook.com/dijiSyncLLC"><i
+							class="ti-facebook mr-2"></i>Facebook</a></li>
+				<li class="list-inline-item"><a href="https://twitter.com/DijiSyncLLC"><i
+							class="ti-twitter mr-2"></i>Twitter</a></li>
+				<li class="list-inline-item"><a href="https://www.instagram.com/dijisync/"><i
+							class="ti-instagram mr-2 "></i>Instagram</a></li>
+				<li class="list-inline-item"><a href="https://www.linkedin.com/company/dijisync-hcs-llc/"><i
+							class="ti-linkedin mr-2 "></i>Linkedin</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+</footer>
+
+
+`;
